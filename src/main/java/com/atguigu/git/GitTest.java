81406c83c1d29a9e979dbbe0b03f313440f04255
ZZH <1554687608@qq.com>
2022-10-20 15:43:36 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zzh.java
+ * @create 2022-10-20 15:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
